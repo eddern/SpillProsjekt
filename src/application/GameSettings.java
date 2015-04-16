37f193200695e2bb3ea6d7d9e19b4edb370de14d
Espen Meidell <espen.meidell@gmail.com>
2015-04-16 17:37:19 +0200

    Noe crap

--- src/application/GameSettings.java
+++ src/application/GameSettings.java
@@ -16,25 +16,35 @@ public class GameSettings extends VBox{
 	
 	//Labels
 	private Label titleLabel = new Label("Options");
-	private Label antallLabel = new Label("Spawn i sekundet: ");
+	private Label antallLabel = new Label("Sirkler i sekundet: ");
+	private Label spawnLabel = new Label("Spawn i sekundet: ");
 	
 	//Controls
 	private Slider antallSlider = new Slider(1, 30, 10);
+	private Slider spawnSlider = new Slider(1, 10, 5);
 	
 	public GameSettings() {
+		setSpacing(4);
 		
-		antallLabel.setText("Spawn i sekundet: "+(int)antallSlider.getValue());
-		
-		
+		antallLabel.setText("Sirkler i sekundet: "+(int)antallSlider.getValue());
 		antallSlider.setMinorTickCount(1);
 		antallSlider.setMajorTickUnit(100);
 		antallSlider.setShowTickLabels(true);
 		antallSlider.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
-
 			@Override
 			public void handle(MouseEvent event) {
 				antallLabel.setText("Spawn i sekundet: "+(int)antallSlider.getValue());
-				
+			}
+		});
+		
+		spawnLabel.setText("Spawn i sekundet: "+(int)spawnSlider.getValue());
+		spawnSlider.setMinorTickCount(1);
+		spawnSlider.setMajorTickUnit(100);
+		spawnSlider.setShowTickLabels(true);
+		spawnSlider.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				spawnLabel.setText("Spawn i sekundet: "+(int)spawnSlider.getValue());
 			}
 		});
 		
@@ -45,9 +55,9 @@ public class GameSettings extends VBox{
 		
 		//Adding to parent node
 		getChildren().add(titleLabel);
-		getChildren().add(antallLabel);
-		getChildren().add(antallSlider);
+		getChildren().addAll(antallLabel, antallSlider);
 		getChildren().add(new Separator(Orientation.HORIZONTAL));
+		getChildren().addAll(spawnLabel, spawnSlider);
 	}
 	
 	
